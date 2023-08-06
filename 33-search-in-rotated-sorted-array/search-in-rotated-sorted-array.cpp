b81ebffee1e72b8399879cd80668a9199ddefde8
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-08-05 22:24:39 -0700

    Time: 3 ms (77.20%) | Memory: 11.1 MB (44.44%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,50 +1,49 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
-       int low=0;
-       int high=arr.size()-1;
-       int mid=(low+high)/2;
-        // Find the Sorted Part
+    int search(vector<int>& nums, int target) {
+        //1.Identify the Sorted Half
+        int low=0;
+        int high=nums.size()-1;
+        int mid=(low+high)/2;
         while(low<=high)
         {
-            // Element found
-            if(arr[mid]==target)
-            return mid;
-            // Elemination 
-            // If the Left part is Sorted
-            if(arr[low]<=arr[mid])
+        // Element Found
+        if(nums[mid]==target)
+        return mid;
+        // Left Part is Sorted
+        else if(nums[low]<=nums[mid])
+        {
+            // Check if Element is Present in The Left Part or Not
+            if(nums[low]<=target && nums[mid]>=target)
             {
-                // Check if taregt is Present in the Half or not
-                if(arr[low]<=target && arr[mid]>=target)
-                {
-                    // Present ->Move to Left Part
-                    high=mid-1;
-                }
-                // Not Present -> Move to Right Part
-                else
-                {
-                    low=mid+1;
-                }
+                // Move to Left
+                high=mid-1;
             }
-
-            // Right Part is Sorted
-            else{
-                // Check if taregt is Present in the Half or not
-                if(arr[mid]<=target && arr[high]>=target)
-                {
-                    // Present ->Move to Right Part
-                    low=mid+1;
-                }
-                // Not Present -> Move to Left Part
-                else
-                {
-                    high=mid-1;
-                }
+            // Move to Right
+            else
+            {
+                low=mid+1;
             }
-            // Update Mid
-            mid=(low+high)/2;
         }
-        // element is not present
+        // Right Part is Srted
+        else 
+        {
+            // Check if Element is Present in The Right Part or Not
+            if(nums[mid]<=target && nums[high]>=target)
+            {
+                // Move to Right
+                low=mid+1;
+            }
+            // Move to Left
+            else
+            {
+                high=mid-1;
+            }
+        }
+        // Update Mid
+        mid=(low+high)/2;
+        }
+        // Element is Not Found
         return -1;
     }
 };
