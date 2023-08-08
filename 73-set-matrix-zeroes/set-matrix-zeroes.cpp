ec945dc442ca51acfab6fb22e76fa9bf71375748
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-08-08 03:42:30 -0700

    Time: 12 ms (85.90%) | Memory: 13.6 MB (11.43%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& mattrix) {
+        /*
+        // Approch 1:
         int n=mattrix.size();
         int m=mattrix[0].size();
-        // Set Row & Column to -1
+        // Set Row & Column to 1e9
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(mattrix[i][j]==0)
                 {
-                    // Set entire Row -1
+                    // Set entire Row 1e9
                     for(int k=0;k<n;k++)
                     {
                         if(mattrix[k][j]!=0 && mattrix[k][j]!=1e9)
                         mattrix[k][j]=1e9;
                     }
-                    // Set entire Column -1
+                    // Set entire Column 1e9
                     for(int k=0;k<m;k++)
                     {
                         if(mattrix[i][k]!=0 && mattrix[i][k]!=1e9)
@@ -25,7 +27,7 @@ public:
                 }
             }
         }
-        // Change -1 to 0
+        // Change 1e9 to 0
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
@@ -35,6 +37,40 @@ public:
                     mattrix[i][j]=0;
                 }
             }
+        }
+    }
+    */
+
+    // Approch 2: Stack/Queue Approch
+    int n=mattrix.size();
+    int m=mattrix[0].size();
+    // 1.Create a Queue(row,col)
+    queue<pair<int,int>>q;
+    // 2.Traverse Whole Matrix & insert the (row,col) of 0
+    for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(mattrix[i][j]==0)
+                {
+                    // Push row & Column into Queue
+                    q.push({i,j});
+                }
+            }
+        }
+        // 3.Take out Elements from Queue & Make Row & Column 0
+        while(!q.empty())
+        {
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            
+            // 3.a.Make Row 0
+            for(int k=0;k<m;k++)
+            mattrix[row][k]=0;
+             // 3.a.Make Column 0
+            for(int k=0;k<n;k++)
+            mattrix[k][col]=0;
         }
     }
 };
