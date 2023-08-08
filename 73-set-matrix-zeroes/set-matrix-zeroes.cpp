87ceb430d1b6c5a0b2c42c8c02f3949854927daa
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-08-08 05:20:59 -0700

    Time: 13 ms (73.20%) | Memory: 13.4 MB (38.67%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -73,6 +73,7 @@ public:
             mattrix[k][col]=0;
         }
         */
+    /*
     // Approch 3: Row& Column Approch
     int n=mattrix.size();
     int m=mattrix[0].size();
@@ -101,6 +102,62 @@ public:
                 mattrix[i][j]=0;
             }
         }
+        */
+        // Optimal Solution
+        /*
+        Row Array-> matrix[0][0-n]
+        Column Array-> matrix[1-m][0]
+        matrix[0][0]->col0
+        */
+        int n=mattrix.size();
+        int m=mattrix[0].size();
+        int col0=1;
+        // 1.Traverse The Matrix & Mark Row,Column Array According to it
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                // if any element is Zero
+                if(mattrix[i][j]==0)
+                {
+                    // a.Mark i th Row 0
+                    mattrix[i][0]=0;
 
+                    // Mark j th Column 0 but check-
+                    // 1.if range of j is btween 1-M then Col Array else col0 variable
+                    if(j!=0)
+                       mattrix[0][j]=0; 
+                    else
+                        col0=0;
+                }
+            }
+        }
+        // 2.Re-Traverse matrix except from First Row(Col Array) & First Column(Row Array) & Convert into Zero According to Row,Col Array
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                // Only Check for NonZero Element
+                if(mattrix[i][j]!=0)
+                {
+                // Check 
+                if(mattrix[i][0]==0 || mattrix[0][j]==0)
+                    mattrix[i][j]=0;
+                }
+            }
+        }
+        // 3.Separately Traverse First Column (First) then First Row(second)
+        // a.if the 0th Row is Zero then all the elements in First Column convert into zero
+        if(mattrix[0][0]==0)
+        {
+            for(int j=0;j<m;j++)
+                mattrix[0][j]=0;
+        }
+        // b.if the col0(0th Column) is Zero then all the elements in First Row convert     into zero
+        if(col0==0)
+        {
+            for(int i=0;i<n;i++)
+                mattrix[i][0]=0;
+        }
     }
 };
