0e713d6dbfcab0ba932d1a5f0c3c83949535f38a
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-08-28 08:28:09 -0700

    Time: 0 ms (100.00%) | Memory: 6.9 MB (25.78%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,29 +1,38 @@
 class MyStack {
 public:
     queue<int>q;
+    queue<int>q_ref;
     MyStack() {
-        
+
     }
     
     void push(int x) {
-        // 1.Push the element in the queue.
-    q.push(x);
-    int n=q.size()-1;
-    // 2.remove element from queue and again push back to the queue
-    while(n--)
-    {
-        q.push(q.front());
-        q.pop();
-    }    
+        // 1.Put Elements From Main Queue(MyStack) to Reference Queue
+        while(!q.empty())
+        {
+            q_ref.push(q.front());
+            q.pop();
+        }
+        // 2.Push Element(X) in Main Queue(MyStack)
+        q.push(x);
+        // 3.Put Back all Elements From Reference Queue to Main Queue(MyStack)
+        while(!q_ref.empty())
+        {
+            q.push(q_ref.front());
+            q_ref.pop();
+        }
     }
     
     int pop() {
-        int ele=q.front();
+        // Simply pop from Main Queue
+        int ele=top();
         q.pop();
         return ele;
+        
     }
     
     int top() {
+        // Simply Return Main Queue front
         return q.front();
     }
     
