e05f1f07ad200e46f86182c3d1d0141dbbf4bff5
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-09-15 21:05:23 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Detect Loop in linked list/detect-loop-in-linked-list.cpp
@@ -0,0 +1,118 @@
+//{ Driver Code Starts
+//Initial template code for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/*
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+        // your code here
+        //  Tortoise-Hare Approch
+        // Intutuion-We can prove that if the Linked List has a Cycle/Loop then fast &  slow Pointer eventually meet at some point for sure. 
+        // Base Case
+        if(head==NULL || head->next==NULL)
+        return false;
+        if(head->next==head)
+        return true;
+        // 1.Create slow Pointer pointing at Head
+        Node* slow=head;
+        // 2.Create fast Pointer pointing at Head
+        Node* fast=head;
+        // 3.Traverse Both the Pointer till one of them becomes NULL-
+        while(slow!=NULL && fast!=NULL)
+        {
+            //a.Update Slow by 1 Step
+            slow=slow->next;
+            //a.Update fast by 2 Step
+            fast=fast->next;
+            if(fast!=NULL)
+            fast=fast->next;
+
+            // c.Check if Slow & Fast is Pointing at the Same Node,if yes then the LL has a Cycle-> Return True
+            if(slow==fast)
+            return true;
+        }
+        // 4.Since slow/fast one of them becomes NULL so the Lnked List Doesnot Have Any Cycle
+        return false;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        if(ob.detectLoop(head) )
+            cout<< "True\n";
+        else
+            cout<< "False\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
