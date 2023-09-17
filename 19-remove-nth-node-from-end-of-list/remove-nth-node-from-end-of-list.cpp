fadd001bb4474222b843d09aafc9bc2b5111de27
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-09-17 11:46:17 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (6.40%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int count(ListNode* head)
+    {
+        int cnt=0;
+        while(head!=NULL)
+        {
+            cnt++;
+            head=head->next;
+        }
+        return cnt;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+         // Base Case
+           if(head==NULL) 
+           return head;
+           ListNode* temp=head; 
+            // 1.Find The Total Number of Nodes in the Linked List
+           int countNode=count(temp);
+            // 2.If N is greater than Total Number of Nodes then return -1
+           if(n>countNode)
+           return NULL;
+        // 3.If N is Equal to Total Number of Nodes then 
+           if(n==countNode)
+           {
+               head=head->next;
+               return head;
+           }
+            // 3.Find the N from start as N=Total Nodes - N 
+           int cnt=countNode-n;
+
+            //4.Iterate till N!=0
+            ListNode* curr=head;
+            ListNode* prev=head;
+           while(cnt--)
+           {
+            prev=curr;
+            curr=curr->next;
+           }
+
+            //5.When N becomes 0 it is Our Nth Node from Last
+            if(prev!=NULL)
+           prev->next=curr->next;
+           curr->next=NULL;
+           return head;
+    }
+};
