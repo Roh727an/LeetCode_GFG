8901575c9b6492045bee79fd90b9fd08f80fd87c
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-10-26 11:33:02 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (37.08%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+// RECURSION
+    int rec(int n)
+    {
+        // Base Case
+        if(n==1 || n==0)
+        return 1;
+
+        return rec(n-1)+rec(n-2);
+    }
+    // MEMOIZATION
+    int Memo(int n,vector<int>dp)
+    {
+        // Base Case
+        if(n==1 || n==0)
+        return 1;
+        if(dp[n]!=-1)
+        return dp[n];
+
+        return dp[n]=Memo(n-1,dp)+Memo(n-2,dp);
+    }
+    int climbStairs(int n) {
+        // RECURSION
+        // return rec(n);
+
+        // MEMOIZATION
+        vector<int>dp(n+1,-1);
+        // return Memo(n,dp);
+        // TABULATION
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
