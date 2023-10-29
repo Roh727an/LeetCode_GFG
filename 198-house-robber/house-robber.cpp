5f0e2b3e6105321561e14350d8227e9938c22d94
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-10-29 22:49:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (48.59%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -18,8 +18,23 @@ int memo(int idx,vector<int>& nums,vector<int>& dp)
 }
     int rob(vector<int>& nums) {
         // MEMOIZATION
-        int n=nums.size();
+    int n=nums.size();
     vector<int>dp(n+1,-1);
-    return memo(n-1,nums,dp);
+    // return memo(n-1,nums,dp);
+    // TABULATION
+    dp[0]=nums[0];
+    dp[1]=0;
+    for(int i=1;i<n;i++)
+    {
+        int pick=nums[i];
+        // Edge Case
+        if(i>1)
+        pick+=dp[i-2];
+
+        int nonPick=0+dp[i-1];
+
+        dp[i]=max(pick,nonPick);
+    }
+    return dp[n-1];
     }
 };
