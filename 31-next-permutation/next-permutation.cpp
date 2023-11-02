df1f5ef8616b8ef7a903f16d6956c1f4e9f8c9e7
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-11-02 17:02:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (40.59%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,41 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        // STL
+        // next_permutation(nums.begin(),nums.end());
+        // return nums;
+        // IMPLEMENTATION OF STL
+        
+        // 1.Find The Break Point
+        int idx=-1;
+        int N=nums.size();
+        for(int i=N-2;i>=0;i--)
+        {
+            if(nums[i] < nums[i+1])
+            {
+                idx=i;
+                break;
+            }
+        }
+        
+        // Edge Case-> IF there is No Break Point
+        if(idx==-1)
+        {
+            reverse(nums.begin(),nums.end());
+            return ;
+        }
+        
+        // 2.Swap the Least Greater Element from idx+1 to N 
+        for(int i=N-1;i>idx;i--)
+        {
+            if(nums[idx] < nums[i])
+            {
+                swap(nums[idx],nums[i]);
+                break;
+            }
+        }
+        
+        // 3.Reverse The Remaing Part from idx+1 to N
+        reverse(nums.begin()+idx+1,nums.end());
     }
 };
