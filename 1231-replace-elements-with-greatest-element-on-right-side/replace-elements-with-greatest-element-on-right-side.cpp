17d9e73f6767ae18f8a90827cd069116c797fb2d
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-11-02 17:35:49 +0530

    Time: 79 ms (74.10%) | Memory: 72.8 MB (5.69%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,46 +1,16 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        // Stack Approch -> T.C = O(2N) & S.C= O(N)
-        if(arr.size() <= 1)
-        return {-1};
-        vector<int>ans;
-        // Create a Stack
-        stack<int>st;
-        // Push -1 as Last Element Does not have any Right
-        st.push(-1);
-        // My Stack will Contain Ultimate Greater of Right So,
-        for(int i=arr.size()-1;i>0;i--)
+        vector<int>rmax;
+        int n=arr.size();
+        int maxi=-1;
+        for(int i=n-1;i>=0;i--)
         {
-            // If Current Element is Greater Than Stack top then push Stack Top as We Found New Ultimate Greater
-            if(arr[i]>st.top())
-            st.push(arr[i]);
-            // Else Ultimate Greater will be Our Stack Top
-            else
-            st.push(st.top());
+            rmax.push_back(maxi);
+            if(arr[i]>maxi)
+            maxi=arr[i];
         }
-        // My Stack will Contain Ultimate Greater for Each Array Element
-        while(!st.empty())
-        {
-            ans.push_back(st.top());
-            st.pop();
-        }
-
-        return ans;
-
-        /*
-        // Optimised Approch T.C=O(N) & S.C=O(1)
-        vector<int>ans;
-        int max=-1;
-        for(int i=arr.size()-1;i>=0;i--)
-        {
-            ans.push_back(max);
-            if(arr[i]>max)
-            max=arr[i];
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
-        */
-        
+        reverse(rmax.begin(),rmax.end());
+        return rmax;
     }
 };
