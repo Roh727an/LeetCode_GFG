179bc57cae85dd1437d21e29af002d21690284a5
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-11-02 18:40:24 +0530

    Time: 117 ms (72.73%) | Memory: 70.8 MB (24.97%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,30 +1,62 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        // SORT THE ARRAY APPROCH
-        // Base Case
-        if(nums.size()==0 )
-        return 0;
-        if(nums.size()==1)
-        return 1;
+        // // SORT THE ARRAY APPROCH
+        // // Base Case
+        // if(nums.size()==0 )
+        // return 0;
+        // if(nums.size()==1)
+        // return 1;
 
-        sort(nums.begin(),nums.end());
-        int len=1;
-        int lastSmaller=INT_MIN;
+        // sort(nums.begin(),nums.end());
+        // int len=1;
+        // int lastSmaller=INT_MIN;
+        // int cnt=0;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(lastSmaller==nums[i]-1)
+        //     {
+        //         cnt++;
+        //         lastSmaller=nums[i];
+        //     }
+        //     else if(lastSmaller!=nums[i])
+        //     {
+        //         cnt=1;
+        //         lastSmaller=nums[i];
+        //     }
+        //     len=max(len,cnt);
+        // }
+        // return len;
+        // SET APPROCH
+        int N=nums.size();
+        if(N==0 )
+        return 0;
+        int len=INT_MIN;
         int cnt=0;
-        for(int i=0;i<nums.size();i++)
+        int ele;
+        unordered_set<int>st;
+        // Put all Elements into Set DS
+        for(int i=0;i<N;i++)
         {
-            if(lastSmaller==nums[i]-1)
-            {
-                cnt++;
-                lastSmaller=nums[i];
-            }
-            else if(lastSmaller!=nums[i])
+            st.insert(nums[i]);
+        }
+        // Pick & Element & Search in DS
+        for(auto it:st)
+        {
+            // If it is a Starting Point
+            if(st.find(it-1)==st.end())
             {
                 cnt=1;
-                lastSmaller=nums[i];
+                ele=it;
+                // Calculate the LEngth of Sequence
+                while(st.find(ele+1)!=st.end())
+                {
+                    ele=ele+1;
+                    cnt++;
+                }
+                // Update length
+                len=max(len,cnt);
             }
-            len=max(len,cnt);
         }
         return len;
     }
