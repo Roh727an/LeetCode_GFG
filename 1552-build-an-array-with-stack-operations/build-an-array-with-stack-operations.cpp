75346e4f3e87c36046421931feb77f90ff1f1f0e
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-11-03 16:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (75.36%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
+        // 
         vector<string>op;
-        // vector<int>temp;
+        // Index pointer for traversing Target Array
         int idx=0;
+        // Iterate from 1 to N
         for(int i=1;i<=n;i++)
         {
+            // If idx crosses target's Last element then We found Our Operations-> Break
             if(idx>=target.size())
             break;
+
             int ele=i;
-            // temp.push_back(ele);
+            // Push the element
             op.push_back("Push");
+            // If the element is not in our Target then Pop it
             if(ele!=target[idx])
             {
-                // temp.pop_back();
                 op.push_back("Pop");
             }
+            // If it is in our Target array then update idx to next element of Target Array
             else
             idx++;
         }
+        // Return the Operation Vector
         return op;
+        // Time Complexity->O(N)
+        // Space Complexity-> O(1)
     }
 };
