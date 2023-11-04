9574cf81242d0428620bcb9ef69c95744998018b
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-11-04 16:48:52 +0530

    Time: 8 ms (68.57%) | Memory: 16.2 MB (64.66%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         // APPROCH -> HASHMAP
+        /*
         int n=nums.size();
         // Store the Frequency of Elements
         unordered_map<int,int>mp;
@@ -15,6 +16,49 @@ public:
             if(it.second > n/3 )
             ans.push_back(it.first);
         }
+        return ans;
+        */
+        // Bayer Moore's Vooting Algorithm Extension
+        int cnt1=0,cnt2=0,ele1=INT_MAX,ele2=INT_MAX;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(cnt1==0 && nums[i]!=ele2)
+            {
+                cnt1=1;
+                ele1=nums[i];
+            }
+            else if(cnt2==0 && nums[i]!=ele1)
+            {
+                cnt2=1;
+                ele2=nums[i];
+            }
+            else if(nums[i]==ele1)
+            cnt1++;
+            else if(nums[i]==ele2)
+            cnt2++;
+            else
+            {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        // Manually Check
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==ele1)
+            cnt1++;
+            else if(nums[i]==ele2)
+            cnt2++;
+
+        }
+        vector<int>ans;
+        if(cnt1> nums.size()/3)
+        ans.push_back(ele1);
+        if(cnt2> nums.size()/3)
+        ans.push_back(ele2);
+
         return ans;
     }
 };
