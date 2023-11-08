d3be3ddd447624869d84f9bdedc8b08e54513e59
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-11-08 12:03:47 +0530

    Time: 3 ms (33.86%) | Memory: 9.4 MB (35.33%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,37 +1,59 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        /*
     //    Base Case
-        if(m==0)
-        {
-        nums1=nums2;
-        return ;
+    if (m == 0) {
+        nums1 = nums2;
+        return;
+    }
+    
+    int i = 0;
+    int j = 0;
+    vector < int > ans;
+    // Iterate Till Anyone Goes out of Bound
+    while (i < m && j < n) {
+        if (nums1[i] <= nums2[j]) {
+            ans.push_back(nums1[i]);
+            i++;
         }
-       
-       int i=0;
-       int j=0; 
-       vector<int>ans;
-       while(i<m && j<n){
-           if(nums1[i]<=nums2[j])
-           {
-               ans.push_back(nums1[i]);
-               i++;
-           }
-           else{
-               ans.push_back(nums2[j]);
-               j++;
-           }
-       }
-    //    Push Remaining Elements
-    while(i<m ){
+        else {
+            ans.push_back(nums2[j]);
+            j++;
+        }
+    }
+    // Push Remaining Elements
+    while (i < m) {
         ans.push_back(nums1[i]);
-         i++; 
-       }
-    while(j<n){
+        i++;
+    }
+    while (j < n) {
         ans.push_back(nums2[j]);
-         j++; 
-       }
-    nums1=ans;
+        j++;
+    }
+    nums1 = ans;    
+    */
+    // OPTIMISED APPROCH
+    // Base Case
+    if (m == 0) {
+        nums1 = nums2;
+        return;
+    }
+    int i=0,j=0;
+
+    while(i<m && j<n)
+    {
+        if(nums1[i] > nums2[j]){
+            swap(nums1[i],nums2[j]);
+            sort(nums2.begin(),nums2.end());
+        }
+        i++;
+    }    
+    for(int j=0;j<n;j++)
+    nums1[i+j]=(nums2[j]);
+
+
+
 
     }
 };
