b07923c19532816331cf92aeb1b0bc2591c89389
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-11-10 17:09:41 +0530

    Time: 7 ms (36.13%) | Memory: 14 MB (73.86%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        //  Start to End KADANE'S ALGORITHM
+        // APPROCH 1 -> PREFIX & SUFFIX
+        /*
+        int pre=1;
+        int suf=1;
+        int prod=INT_MIN;
         int n=nums.size();
-        int sum=1;
-        int maxSum=INT_MIN;
         for(int i=0;i<n;i++)
         {
-            sum*=nums[i];
-            // Update
-            if(sum>maxSum)
-            maxSum=sum;
+            // Previous Element was 0 -> Reset pre & suf
+            if(pre==0)
+            pre=1;
+            if(suf==0)
+            suf=1;
+            // Calculate pre &suf
+            pre*=nums[i];
+            suf*=nums[n-1-i];
 
-            // Reset
-            if(sum==0)
-            sum=1;
+            // Calculate Max Subarray Product
+            prod=max(prod,max(pre,suf));
         }
-        //  End to Start KADANE'S ALGORITHM
-        sum=1;
-        for(int i=n-1;i>=0;i--)
-        {
-            sum*=nums[i];
-            // Update
-            if(sum>maxSum)
-            maxSum=sum;
+        return prod;
+        */
+        // APPROCH 2 -> KADANE'S ALGORITHM (EXTENSION)
+        int prod1=nums[0],prod2=nums[0],ans=nums[0];
 
-            // Reset
-            if(sum==0)
-            sum=1;
+        for(int i=1;i<nums.size();i++){
+            int temp=max({prod1*nums[i],prod2*nums[i],nums[i]});
+            prod2=min({prod1*nums[i],prod2*nums[i],nums[i]});
+            prod1=temp;
+            ans=max(ans,prod1);
         }
-        return maxSum;
+        return ans;
     }
 };
