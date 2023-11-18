551fa6e2a0be0076a3a4d4fd319033239eb177d8
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-11-18 23:28:15 +0530

    Updated Solution - GfG to GitHub

--- Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.cpp
+++ Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -14,7 +14,7 @@ class Solution{
     double MedianOfArrays(vector<int>& nums1, vector<int>& nums2)
     {
         // Your code goes here
-        // APPROCH 1 -> MERGE & FIND MEDIAN 
+                // APPROCH 1 -> MERGE & FIND MEDIAN 
         /*
         int n1=nums1.size(),n2=nums2.size();
         vector<int>merge;
@@ -42,6 +42,7 @@ class Solution{
         return (double)ele2;
         */
         // APPROCH 2 -> OPTIMIZATION OF APPROCH 1
+        /*
         int n1=nums1.size(),n2=nums2.size();
         int n=n1+n2;
         int ele1,ele2,idx2=n/2,idx1=n/2 - 1,cnt=0;
@@ -89,7 +90,56 @@ class Solution{
         return ((double)ele2 + (double)ele1)/2.0;
         else
         return (double)ele2;
-        
+        */
+        // APPROCH 3 -> BINARY SEARCH
+
+        int n1=nums1.size();
+        int n2=nums2.size();
+        // We consider Array 1 is Smaller So We call Same Function with Swaped Array 
+        if(n1>n2)
+        return MedianOfArrays(nums2,nums1);
+        //TOTAL LENGTH
+        int n=n1+n2;
+        // Length of LEFT PART
+        int left=(n1+n2+1)/2;
+
+        // BINARY SEARCH
+        int low=0;
+        int high=n1;
+        while(low<=high){
+            // Calculate Mid
+            // mid1 -> Middle of Array 1 -> X elements from Array 1 => Value of X
+            int mid1=(low+high)>>1;
+            // mid2 -> left-X elements from Array 2 => Value of Y
+            int mid2=left-mid1;
+
+            // We found our mid , Now we can calculate l1,l2,r1 & r2
+            int l1=INT_MIN,l2=INT_MIN;
+            int r1=INT_MAX,r2=INT_MAX;
+            // Check Range & Put Value from A1 & A2
+            if(mid1 < n1) r1=nums1[mid1];
+            if(mid2 < n2) r2=nums2[mid2];
+            if(mid1 - 1 >= 0) l1=nums1[mid1-1];
+            if(mid2 - 1 >= 0) l2=nums2[mid2-1];
+
+            // Eliminate Halfs based on l1,l2,r1 & r2
+            // ANS FOUND(MIDDLE FOUND)
+            if(l1<=r2 && l2<=r1)
+            {
+                // (TOTAL LENGTH) ODD CASE
+                if(n%2!=0)
+                return max(l1,l2);
+                // (TOTAL LENGTH)EVEN CASE
+                else
+                return (double)(max(l1,l2)+min(r1,r2))/2.0;
+            }
+            // ELIMINATE RIGHT
+            if(l1>r2)
+            high=mid1-1;
+            else
+            low=mid1+1;
+        }
+        return 0;
     
     }
 };
