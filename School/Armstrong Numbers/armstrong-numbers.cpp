1392c3223bcae5f6bf88457fc7a1b7bb09ce4821
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-12-01 17:13:26 +0530

    Added Solution - GfG to GitHub

---
+++ School/Armstrong Numbers/armstrong-numbers.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string armstrongNumber(int n){
+        // code here
+        int originalno = n;
+    int count = 0;
+    int temp = n;
+    while (temp != 0)
+    {
+        count++;
+        temp = temp / 10;
+    }
+    int sumofpower = 0;
+    while (n != 0)
+    {
+        int digit = n % 10;
+        sumofpower += pow(digit,count);
+        n /= 10;
+    }
+    if(sumofpower == originalno)
+    return "Yes";
+    else
+    return "No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
