5ddaa2518669ad4bbb3b3942871782dea8434642
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-12-08 09:35:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Transform to prime/transform-to-prime.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    int findPrime(int n){
+        while(true){
+            int temp=false;
+        for(int i=2;i<=sqrt(n);i++)
+        {
+            if(n%i==0)
+            temp=true;
+        }
+        if(!temp)
+        break;
+        n++;
+        }
+        return n;
+    }
+    int minNumber(int arr[],int N)
+    {
+        long sum=0;
+        for(int i=0;i<N;i++)
+        sum+=arr[i];
+        
+        return findPrime(sum)-sum;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution obj;
+        cout << obj.minNumber(arr, n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
