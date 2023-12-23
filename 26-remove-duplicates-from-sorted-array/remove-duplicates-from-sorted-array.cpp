193b7d1d9405cb7ada0776d77031bcc4e5fa8ccd
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-12-23 11:08:51 +0530

    Time: 7 ms (77.93%) | Memory: 18.9 MB (29.82%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int distinct_element_index=0;
-        int iteration_index=0;
-        while(iteration_index<nums.size())
+        vector<int>nonDup;
+        nonDup.push_back(nums[0]);
+        // Push all NonDuplicates into nonDup Array
+        for(int i=1;i<nums.size();i++)
         {
-            // If distinct ELement Found
-            if(nums[distinct_element_index]!=nums[iteration_index])
-            {
-                nums[distinct_element_index+1]=nums[iteration_index];
-                // Update Distinct ELement
-                distinct_element_index++;
-            }
-            // Update iteration index
-            else
-            iteration_index++;
+            if(nonDup.back()!=nums[i])
+            nonDup.push_back(nums[i]);
         }
-        return  distinct_element_index+1;
+        // Put back all non duplicates in front of array
+        int k=0;
+        for(k=0;k<nonDup.size();k++){
+            nums[k]=nonDup[k];
+        }
+        return k;
     }
 };
