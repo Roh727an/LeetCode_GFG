ff94fa38e9d217e6f88cf12c8c7ce5658b2aa836
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-12-23 12:21:26 +0530

    Time: 14 ms (67.62%) | Memory: 17.3 MB (49.13%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        // Xor all elements in the array 
-        // Xor will cut elements that apearing 2 times as a^a=0 & 0^b=b
-        int ans=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            ans^=nums[i];
-        }
-        return ans;
+       int singleElement=0;
+       for(int i=0;i<nums.size();i++)
+       singleElement=singleElement^nums[i];
+
+       return singleElement;
     }
 };
