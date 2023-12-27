b303830ae50af8b4ce8ce7edffffa5c2fdaf4301
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-12-27 16:55:55 +0530

    Time: 3 ms (66.30%) | Memory: 11.4 MB (68.54%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        //1.Identify the Sorted Half
-        int low=0;
-        int high=nums.size()-1;
-        int mid=(low+high)/2;
-        while(low<=high)
+    int search(vector<int>& arr, int target) {
+      int n=arr.size();
+      int s=0;
+      int e=n-1;
+      int mid=(s+e)/2;
+      while(s<=e)
+      {
+          if(arr[mid]==target)
+          return mid;
+
+        // Left Half is Sorted
+        if(arr[s]<=arr[mid])
         {
-        // Element Found
-        if(nums[mid]==target)
-        return mid;
-        // Left Part is Sorted
-        else if(nums[low]<=nums[mid])
-        {
-            // Check if Element is Present in The Left Part or Not
-            if(nums[low]<=target && nums[mid]>=target)
-            {
-                // Move to Left
-                high=mid-1;
+            // IF the ELement is In the Range or Not
+            if(arr[s]<=target && arr[mid]>=target){
+                e=mid-1;
             }
-            // Move to Right
             else
-            {
-                low=mid+1;
-            }
+            s=mid+1;
         }
-        // Right Part is Srted
-        else 
-        {
-            // Check if Element is Present in The Right Part or Not
-            if(nums[mid]<=target && nums[high]>=target)
-            {
-                // Move to Right
-                low=mid+1;
+        // Right Half is Sorted
+        else{
+            if(arr[mid]<=target && arr[e]>=target){
+                s=mid+1;
             }
-            // Move to Left
             else
-            {
-                high=mid-1;
-            }
+            e=mid-1;
         }
-        // Update Mid
-        mid=(low+high)/2;
-        }
-        // Element is Not Found
-        return -1;
+        // UPDATE MID
+        mid=(s+e)/2;
+      }
+        
+    return -1;
     }
 };
