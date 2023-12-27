014b8973b07ee4f1f49f5348325bfefa4d46ca4c
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-12-27 17:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (91.56%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        int low=0;
-        int high=nums.size()-1;
-        int mid=(low+high)/2;
-        while(low<=high)
-        {
-            if(nums[mid]==target) 
-            return true;
+    bool search(vector<int>& arr, int target) {
+    int n=arr.size();
+      int s=0;
+      int e=n-1;
+      int mid=(s+e)/2;
+      while(s<=e)
+      {
+          if(arr[mid]==target)
+          return true;
 
-            // Shrink Search Space
-            if(nums[mid]==nums[low] && nums[mid]==nums[high])
-            {
-            low++;
-            high--;
+        // Minimize Search Space by Skipping Duplicates
+        if(arr[s]==arr[mid] && arr[mid]==arr[e])
+        {
+            s++;
+            e--;
             continue;
-            }
-            
-            // Left Side Sorted
-            if(nums[low]<=nums[mid])
-            {
-                if(nums[low]<=target && nums[mid]>=target)
-                high=mid-1;
-                else
-                low=mid+1;
-            }
-            else{
-                if(nums[mid]<=target && nums[high]>=target)
-                low=mid+1;
-                else
-                high=mid-1;
-            }
-             mid=(low+high)/2;
         }
-        return false;
+        // Left Half is Sorted
+        if(arr[s]<=arr[mid])
+        {
+            // IF the ELement is In the Range or Not
+            if(arr[s]<=target && arr[mid]>=target){
+                e=mid-1;
+            }
+            else
+            s=mid+1;
+        }
+        // Right Half is Sorted
+        else{
+            if(arr[mid]<=target && arr[e]>=target){
+                s=mid+1;
+            }
+            else
+            e=mid-1;
+        }
+        // UPDATE MID
+        mid=(s+e)/2;
+      }
+        
+    return false;
     }
 };
