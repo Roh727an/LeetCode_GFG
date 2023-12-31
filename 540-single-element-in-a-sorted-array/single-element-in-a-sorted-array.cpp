4566fdc3cfd4524d92ecdb1642561b297dd0f626
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-12-31 17:44:23 +0530

    Time: 54 ms (5.36%) | Memory: 22.8 MB (22.61%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,10 +1,37 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            ans^=nums[i];
+    int singleNonDuplicate(vector<int>& arr) {
+    //size of the array.
+    int n = arr.size(); 
+
+    //Edge cases:
+    if (n == 1) return arr[0];
+    if (arr[0] != arr[1]) return arr[0];
+    if (arr[n - 1] != arr[n - 2]) return arr[n - 1];
+
+    int low = 1, high = n - 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if arr[mid] is the single element:
+        if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1]) {
+            return arr[mid];
         }
-        return ans;
+
+        //we are in the left:
+        if ((mid % 2 == 1 && arr[mid] == arr[mid - 1])
+                || (mid % 2 == 0 && arr[mid] == arr[mid + 1])) {
+            //eliminate the left half:
+            low = mid + 1;
+        }
+        //we are in the right:
+        else {
+            //eliminate the right half:
+            high = mid - 1;
+        }
+    }
+
+    // dummy return statement:
+    return -1;
     }
 };
