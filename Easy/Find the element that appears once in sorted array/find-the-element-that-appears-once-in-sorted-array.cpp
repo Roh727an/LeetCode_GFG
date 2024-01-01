213e3702ea09820205fa2a156bb6ec55242e782a
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-01-01 18:39:29 +0530

    Updated Solution - GfG to GitHub

--- Easy/Find the element that appears once in sorted array/find-the-element-that-appears-once-in-sorted-array.cpp
+++ Easy/Find the element that appears once in sorted array/find-the-element-that-appears-once-in-sorted-array.cpp
@@ -9,37 +9,30 @@ using namespace std;
 class Solution
 {
   public:
-    int findOnce(int arr[], int n)
+    int findOnce(int A[], int n)
     {
-    //Edge cases:
-    if (n == 1) return arr[0];
-    if (arr[0] != arr[1]) return arr[0];
-    if (arr[n - 1] != arr[n - 2]) return arr[n - 1];
-
-    int low = 1, high = n - 2;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-
-        //if arr[mid] is the single element:
-        if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1]) {
-            return arr[mid];
-        }
-
-        //we are in the left:
-        if ((mid % 2 == 1 && arr[mid] == arr[mid - 1])
-                || (mid % 2 == 0 && arr[mid] == arr[mid + 1])) {
-            //eliminate the left half:
-            low = mid + 1;
-        }
-        //we are in the right:
-        else {
-            //eliminate the right half:
-            high = mid - 1;
-        }
-    }
-
-    // dummy return statement:
-    return -1;
+        //code here.
+        //Base Cases
+	    if(n==1)
+	    return A[0];
+	    if(A[0]!=A[1])
+	    return A[0];
+	    if(A[n-1]!=A[n-2])
+	    return A[n-1];
+	    
+	    int s=1;
+	    int e=n-2;
+	    while(s<=e){
+	        int mid=(s+e)/2;
+	        if(A[mid]!=A[mid-1] && A[mid]!=A[mid+1])
+	        return A[mid];
+	        
+	        if((mid%2==0 && A[mid]==A[mid+1]) ||(mid%2==1 && A[mid]==A[mid-1]))
+	        s=mid+1;
+	        else
+	        e=mid-1;
+	    }
+	    return -1;
     }
 };
 
