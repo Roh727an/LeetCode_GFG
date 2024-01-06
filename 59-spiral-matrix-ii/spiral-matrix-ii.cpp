b6ae656391bfa1cd5f258b0e1ccc86bc44f27fc5
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-01-06 12:05:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (21.40%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -1,50 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        int left=0,top=0,bottom=n-1,right=n-1;
-        vector<vector<int>>matrix(n,vector<int>(n,0));
-        int cnt=1;
-        while(left<=right && top<=bottom)
+        int r1=0,c1=0,r2=n-1,c2=n-1;
+        vector<vector<int>>matrix(n,vector<int>(n,-1));
+        int ele=1;
+        while(r1<=r2 && c1<=c2)
         {
-
-        // Right Direction Print
-        for(int i=left;i<=right;i++)
-        {
-            matrix[top][i]=cnt;
-            cnt++;
-        }
-        top++;
-
-        // Downwards Direction
-        for(int i=top;i<=bottom;i++)
-        {
-            matrix[i][right]=cnt;
-            cnt++;
-        }
-        right--;
-
-        // Left Direction
-        if(top<=bottom) //check if it is single row or not
-        {
-            for(int i=right;i>=left;i--)
+            // C1 - C2
+            for(int i=c1;i<=c2;i++)
             {
-                matrix[bottom][i]=cnt;
-                cnt++;
+                matrix[r1][i]=ele;
+                ele++;
             }
-            bottom--;
-        }
-        
-        // Upward Direction
-        if(left<=right) //Check if it is single column or not
-        {
-            for(int i=bottom;i>=top;i--)
+            r1++;
+            // R1 - R2
+            for(int i=r1;i<=r2;i++)
             {
-               matrix[i][left]=cnt;
-               cnt++;
+                matrix[i][c2]=ele;
+                ele++;
             }
-            left++;
+            c2--;
+            // We only traverse if it has at least a sningle row
+            if(r1<=r2){
+            // C2 - C1
+            for(int i=c2;i>=c1;i--)
+            {
+                matrix[r2][i]=ele;
+                ele++;
+            }
+            r2--;
+            }
+
+            // We only traverse if it has at least a sningle row
+            if(c1<=c2)
+            {
+            // R2 - R1
+            for(int i=r2;i>=r1;i--)
+            {
+                matrix[i][c1]=ele;
+                ele++;
+            }
+            c1++;
+            }
         }
-        }
-        return matrix;
+        return matrix; 
     }
 };
