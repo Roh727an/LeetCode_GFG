46019702c36d4902d74759bf568e97783bf97503
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-01-06 17:10:37 +0530

    Time: 25 ms (72.92%) | Memory: 13.5 MB (78.74%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+        /*
         int n=matrix.size();
         int m=matrix[0].size();
         priority_queue<int>hp;
@@ -11,8 +12,26 @@ public:
                 hp.push(matrix[i][j]);
                 if(hp.size()>k)
                 hp.pop();            
+                cout<<matrix[i][j]<<" "<<hp.top()<<endl;
             }
         }
         return hp.top();
+        */
+        int n=matrix.size();
+        int low=matrix[0][0],high=matrix[n-1][n-1];
+        while(low<high){
+            int mid=low+(high-low)/2;
+            int cntSmall=0;
+            for(int i=0;i<n;i++)
+            {
+                int upb=upper_bound(matrix[i].begin(),matrix[i].end(),mid) - matrix[i].begin();
+                cntSmall+=upb;
+            }
+            if(cntSmall < k)
+            low=mid+1;
+            else
+            high=mid;
+        }
+        return low;
     }
 };
