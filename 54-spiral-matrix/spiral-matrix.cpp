cdaab08470f7ec64d502a52083dda7b5fa9a0329
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-01-06 11:58:24 +0530

    Time: 3 ms (40.46%) | Memory: 7.2 MB (60.96%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -3,45 +3,43 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        int left=0,top=0,bottom=n-1,right=m-1;
-        vector<int>order;
-        while(left<=right && top<=bottom)
+        int r1=0,c1=0,r2=n-1,c2=m-1;
+        vector<int>ans;
+        while(r1<=r2 && c1<=c2)
         {
-
-        // Right Direction Print
-        for(int i=left;i<=right;i++)
-        {
-            order.push_back(matrix[top][i]);
-        }
-        top++;
-
-        // Downwards Direction
-        for(int i=top;i<=bottom;i++)
-        {
-            order.push_back(matrix[i][right]);
-        }
-        right--;
-
-        // Left Direction
-        if(top<=bottom) //check if it is single row or not
-        {
-            for(int i=right;i>=left;i--)
+            // C1 - C2
+            for(int i=c1;i<=c2;i++)
             {
-                order.push_back(matrix[bottom][i]);
+                ans.push_back(matrix[r1][i]);
             }
-            bottom--;
-        }
-        
-        // Upward Direction
-        if(left<=right) //Check if it is single column or not
-        {
-            for(int i=bottom;i>=top;i--)
+            r1++;
+            // R1 - R2
+            for(int i=r1;i<=r2;i++)
             {
-                order.push_back(matrix[i][left]);
+                ans.push_back(matrix[i][c2]);
             }
-            left++;
+            c2--;
+            // We only traverse if it has at least a sningle row
+            if(r1<=r2){
+            // C2 - C1
+            for(int i=c2;i>=c1;i--)
+            {
+                ans.push_back(matrix[r2][i]);
+            }
+            r2--;
+            }
+
+            // We only traverse if it has at least a sningle row
+            if(c1<=c2)
+            {
+            // R2 - R1
+            for(int i=r2;i>=r1;i--)
+            {
+                ans.push_back(matrix[i][c1]);
+            }
+            c1++;
+            }
         }
-        }
-        return order;
+        return ans;
     }
 };
