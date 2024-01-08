35efc049f59ad4a741b40168a6492b02c3ab8e2f
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-01-08 17:16:58 +0530

    Time: 100 ms (78.30%) | Memory: 48.3 MB (81.08%) - LeetSync

--- 2728-sum-in-a-matrix/sum-in-a-matrix.cpp
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
-int FindMax(vector<vector<int>>& nums,int n,int col)
-{
-    int mx=-1;
-    for(int i=0;i<n;i++)
-    {
-    mx=max(nums[i][col],mx);
-    nums[i][col]=-1;
-    }
-    
-
-    return mx;
-}
     int matrixSum(vector<vector<int>>& nums) {
         int n=nums.size();
         int m=nums[0].size();
+        int ans=0;
+        // Sort each Row first
         for(int i=0;i<n;i++)
         {
             sort(nums[i].begin(),nums[i].end());
         }
-        // cout<<"Matrix"<<endl;
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=0;j<m;j++)
-        //     cout<<nums[i][j]<<" ";
-        //     cout<<endl;
-        // }
-        int ans=0;
-        for(int i=m-1;i>=0;i--)
+        for(int j=m-1;j>=0;j--)
         {
-            // Pass Whole Column -> Last to First
-            int max_Rows=FindMax(nums,n,i);
-
-            // cout<<max_Rows<<" For "<<i<<endl;
-            ans+=max_Rows;
+            int maxEle=0;
+            for(int i=0;i<n;i++)
+            {
+                maxEle=max(maxEle,nums[i][j]);
+                nums[i][j]=-1;
+            }
+            ans+=maxEle;
         }
-
         return ans;
     }
 };
