a93d0578be11e0f33b2de0a7529588f3b751fe2e
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-01-09 16:58:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (35.78%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int ans=-1;
-        int low=0;
-        int high=nums.size()-1;
-        int mid=(low+high)/2;
-        /*
-               >   >      
-        1 2 3 4 |8| 2 4 5
-        Normally-> arr[mid]<arr[mid+1]
-        But for Peak index-> arr[mid]>arr[mid+1]=> mid can be our Peak Index
-        */
-        while(low<high)
-        {
+        int n=nums.size();
+        // Base
+        // 1.a.If the Array Contains single elemnt then it is a Peak Element
+        if(n==1)
+        return 0;
+        // b.Last element is Peak Element
+        if(nums[n-1] > nums[n-2])
+        return n-1;
+        // c.First Element is Peak Element
+        if(nums[0] > nums[1])
+        return 0;
 
+        // Search Space is 1 to n-2
+        // Binary Search Logic
+        int low=1,high=n-2;
+        while(low<=high)
+        {
+            // Calculate mid
+            int mid=(low+high)/2;
+
+            // Check if Mid is Peak or Not
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])
+            return mid;
+
+            // If mid is in Decreasing Slope then Peak is on the Left Part
             if(nums[mid]>nums[mid+1])
-            {
-                // Left Side include Mid
-                high=mid;
-            }
+            high=mid-1;
+            // Else Peak is on the Right part
             else
-            {
-                // Right Side exclude mid
-                low=mid+1;
-            }
-            // Update Mid
-            mid=(low+high)/2;
+            low=mid+1;
         }
-        return mid;
+        return -1;
     }
 };
