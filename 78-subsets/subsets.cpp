6acb2a76d755c1da4f3b3a79b9d1c81a58ebc333
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-01-15 12:03:33 +0530

    Time: 8 ms (8.29%) | Memory: 16.2 MB (31.15%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    void rec(int i,int n,vector<int>temp,vector<vector<int>>&ans,vector<int>& nums)
+void rec(int i,int n,vector<int>& nums,vector<int>temp,vector<vector<int>>&ans)
+{
+    if(i>=n)
     {
-        // Base Case
-        if(i>=n)
-        {
-            ans.push_back(temp);
-            return ;
-        }
-        // Pick an element
-        temp.push_back(nums[i]);
-        // Recursive Call with picked element
-        rec(i+1,n,temp,ans,nums);
-        // Pop the Element 
-        temp.pop_back();
-        // Recursive without picking element
-        rec(i+1,n,temp,ans,nums);
+        ans.push_back(temp);
+        return;
     }
+    // Pick up
+    temp.push_back(nums[i]);
+    rec(i+1,n,nums,temp,ans);
+    // Backtracks
+    temp.pop_back();
+    // Not Pick
+    rec(i+1,n,nums,temp,ans);
+}
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
         vector<int>temp;
-        rec(0,nums.size(),temp,ans,nums);
+        rec(0,nums.size(),nums,temp,ans);
         return ans;
     }
 };
