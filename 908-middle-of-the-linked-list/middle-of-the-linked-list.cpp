11ef38179f75ae235ec592954f998dbe528e4a8f
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-01-24 11:52:06 +0530

    Time: 4 ms (15.91%) | Memory: 8.7 MB (5.40%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -11,43 +11,18 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        /*
-        // Approch 1
-        // Base Case
-        if(head==NULL)
-        return -1;
-        // 1.Count Number of Nodes Present in Linked List
-        ListNode* temp=head;
-        int NodeCount=countNodes(temp);
-        cout<<"Total Nodes "<<NodeCount<<endl;
-        int mid=NodeCount/2;
-        int cnt=0;
-        // 2.Find Middle Element
-        while(cnt<mid)
-        {
-            head=head->next;
-            cnt++;
-        }
-        return head->data;
-        */
-        // Approch 2->
-        // Base Case
-        if(head==NULL ||head->next==NULL)
+        // Fast Pointer
+        if(head->next==NULL)
         return head;
-        // 1.Take Two Pointer slow & Fast
-        ListNode* slow=head;
         ListNode* fast=head;
-        // 2.Traverse till Not NUll or Last Node
-        while(fast!=NULL && fast->next !=NULL)
+        ListNode* slow=head;
+        while(fast!=NULL && fast->next!=NULL)
         {
-            // a.Move Slow Pointer By One Step
-            slow=slow->next;
-            // b.Move Fast Pointer By Two Step
             fast=fast->next;
             if(fast!=NULL)
             fast=fast->next;
+            slow=slow->next;
         }
-        // 3.When Fast Reaches Last Or NULL Slow is Pointing Middle Node
         return slow;
     }
 };
