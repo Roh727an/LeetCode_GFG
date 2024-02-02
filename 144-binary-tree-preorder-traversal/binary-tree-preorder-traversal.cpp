292a1081c704e09997c041231d6647672b2cecc4
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-02-02 16:39:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (32.35%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -12,6 +12,7 @@
 class Solution {
 public:
     vector<int>order;
+    // Recursion
     void preOrderRec(TreeNode * root)
     {
         // Base Case
@@ -26,7 +27,31 @@ public:
         preOrderRec(root->right);
     }
     vector<int> preorderTraversal(TreeNode* root) {
-        preOrderRec(root);
+        // Recursion
+        // preOrderRec(root);
+
+        // Base Case
+        if(root==NULL)
+        return order;
+        // 1.Create a Stack to Store Nodes
+        stack<TreeNode*>st;
+        // 2.Push the Root node into Stack
+        st.push(root);
+        // 3.Iterate till stack becomes Empty
+        while(!st.empty())
+        {
+            // A.Take out First Node from Stack & Pop it from Stack
+            TreeNode* node=st.top();
+            st.pop();
+            // B.Store the top node value
+            order.push_back(node->val);
+            // C. If the Right Node is not NULL then Push Right Node into Stack First
+            if(node->right!=NULL)
+            st.push(node->right);
+            // D. If the Left Node is not NULL then Push Left Node into Stack Second
+            if(node->left!=NULL)
+            st.push(node->left);
+        }
         return order;
     }
 };
