226ace32656c7b5920f402281aadbd6ddf7c6782
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-02-02 17:33:01 +0530

    Time: 4 ms (33.26%) | Memory: 9.8 MB (19.69%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -12,6 +12,7 @@
 class Solution {
 public:
     vector<int>order;
+    // Recursive Approch
     void inOrderRec(TreeNode* root)
     {
         // Base Case
@@ -25,7 +26,41 @@ public:
         inOrderRec(root->right);
     }
     vector<int> inorderTraversal(TreeNode* root) {
-        inOrderRec(root);
+        // Recursive Approch
+        // inOrderRec(root);
+        // Iterative Approch
+       // Base Case
+        if(root==NULL)
+        return order;
+        // 1.Create a Stack to Store Nodes
+        stack<TreeNode*>st;
+        // 2.Create a traverse node as Root
+        TreeNode* node=root;
+        // 3.Iterate till stack becomes Empty,but initially stack is empty so iterate till true
+        while(true)
+        {
+            // A.If Node is  Not NULL then Push Node into Stack & Move Left
+            if(node!=NULL)
+            {
+                st.push(node);
+                node=node->left;
+            }
+            // B.If Node is  NULL then-
+            else
+            {
+                // i. if Stack is Empty -> Iteration Over
+                if(st.empty())
+                break;
+
+                // ii. take out top node from Stack
+                node=st.top();
+                st.pop();
+                // iii.Store it 
+                order.push_back(node->val);
+                // iv.Move Right of it
+                node=node->right;
+            }
+        }
         return order;
     }
 };
