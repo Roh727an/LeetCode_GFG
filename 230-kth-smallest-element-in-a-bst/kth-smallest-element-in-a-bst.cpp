d19fa1646f360cf3fe39c5ef4bba578c8dd36693
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-02-23 11:18:55 +0530

    Time: 14 ms (56.67%) | Memory: 22.6 MB (66.78%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -11,22 +11,58 @@
  */
 class Solution {
 public:
-void inOrder(TreeNode* root,vector<int>&in){
-    if(root==NULL)
-    return;
-
-    inOrder(root->left,in);
-    in.push_back(root->val);
-    inOrder(root->right,in);
-}
-
-
-    int kthSmallest(TreeNode* root, int k) {
+int rec(TreeNode* root, int &k){
         if(root==NULL)
         return -1;
-        vector<int>in;
-        inOrder(root,in);
 
-        return in[k-1];
+        int left=rec(root->left,k);
+        if(left!=-1)
+        return left;
+
+        k=k-1;
+        if(k==0)
+        return root->val;
+
+        return rec(root->right,k);
+}
+    int kthSmallest(TreeNode* root, int k) {
+        // return rec(root,k);
+        // MORRIS TRAVERSAL
+        int ans=-1;
+        TreeNode* curr = root;
+        while (curr != NULL) {
+            // IF Left Doesnot Exist
+            if (curr->left == NULL) {
+                k--;
+                if(k==0)
+                ans= curr->val;
+
+                curr = curr->right;
+            }
+            // Left Exists
+            else {
+                TreeNode* prev = curr->left;
+                // Find RightMost Node
+                while (prev->right && prev->right != curr) {
+                    prev = prev->right;
+                }
+                // IF Rightmost Node is NULL
+                if (prev->right == NULL) {
+                    // Create Thread & Move Curr's Left
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+                // Thread is already There
+                else {
+                    k--;
+                    if(k==0)
+                    ans= curr->val;
+                    // Remove Thread
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
     }
 };
