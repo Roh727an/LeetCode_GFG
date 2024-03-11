99cc8db96855e8a9bec63a3f132d589ae59a1679
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-03-11 15:21:01 +0530

    Time: 3 ms (50.27%) | Memory: 8.3 MB (30.10%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        // Custom Comparator
-        auto cmp=[&](char a,char b){
-            return order.find(a) < order.find(b);
-        };
-        sort(s.begin(),s.end(),cmp);
-        return s;
+        unordered_map<char,int>freq;
+        for(auto i:s)
+        freq[i]++;
+
+        string ans="";
+        for(auto i:order){
+            while(freq[i]-- > 0)
+            ans.push_back(i);
+        }
+
+        for(auto [i,j]:freq){
+            while(j-- > 0)
+            ans.push_back(i);
+        }
+        return ans;
     }
 };
