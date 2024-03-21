a82576658af6aef4bde08982b9c610510a57b149
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-03-21 10:07:24 +0530

    Time: 3 ms (79.91%) | Memory: 11.5 MB (67.59%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,7 +10,7 @@
  */
 class Solution {
 public:
-    void Reverse(ListNode* &prev,ListNode*&curr){
+void Reverse(ListNode* &prev,ListNode*&curr){
         // Base Case
         if(curr==NULL)
         return ;
@@ -22,41 +22,7 @@ public:
         Reverse(prev,curr);
     }
     ListNode* reverseList(ListNode* head) {
-        // code here
-        // return head of reversed list
-        /*
-        // Approch 1-> Brute Force Approch => T.C=O(N) & S.C=O(N)
-        ListNode* rev_head=new Node(head->data);
-        head=head->next;
-        while(head!=NULL)
-        {
-            InsertAtHead(rev_head,head->data);
-            head=head->next;
-        }
-        return rev_head;
-        */
-        //    OPtimised Approch - T.C=O(N) & S.C=O(1)
-        /*
-        // 1.Create a Prev pointer initially pointing at NULL
-        ListNode* prev = NULL;
-        // 1.Create a Curr pointer initially pointing at head
-        ListNode* curr = head;
-        // 3.Traverse till Curr Pointer becomes NULL
-        while (curr != NULL)
-        {
-            // a.Create a Forward Pointer for Curr's Next Track
-            ListNode* forward = curr->next;
-            // b.Point Currr's Next to Prev
-            curr->next = prev;
-            // c.Update Prev as Curr
-            prev = curr;
-            // d.Update Curr as Forward
-            curr = forward;
-        }
-        // 4.AfterTraversal prev becomes the head of Reverse Linked List
-        return prev;
-        */
-       ListNode* prev=NULL;
+        ListNode* prev=NULL;
        ListNode* curr=head;
        Reverse(prev,curr);
        return prev;
