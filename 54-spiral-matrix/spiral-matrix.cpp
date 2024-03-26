8dc5f9e435b23b0f068308efe61544a79c011b2a
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-03-26 17:04:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (86.97%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int r1=0,c1=0,r2=n-1,c2=m-1;
-        vector<int>ans;
-        while(r1<=r2 && c1<=c2)
-        {
-            // C1 - C2
-            for(int i=c1;i<=c2;i++)
-            {
-                ans.push_back(matrix[r1][i]);
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        vector<int> ans;
+        int r1 = 0, c1 = 0, r2 = mat.size() - 1, c2 = mat[0].size() - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            // First Row -> c1 -> c2
+            for (int i = c1; i <= c2; i++) {
+                ans.push_back(mat[r1][i]);
             }
             r1++;
-            // R1 - R2
-            for(int i=r1;i<=r2;i++)
-            {
-                ans.push_back(matrix[i][c2]);
+
+            // Last Column -> r1 -> r2
+            for (int i = r1; i <= r2; i++) {
+                ans.push_back(mat[i][c2]);
             }
             c2--;
-            // We only traverse if it has at least a sningle row
-            if(r1<=r2){
-            // C2 - C1
-            for(int i=c2;i>=c1;i--)
-            {
-                ans.push_back(matrix[r2][i]);
-            }
-            r2--;
+
+            // Last Row-> c2 -> c1
+            if (r1 <= r2) {
+                for (int i = c2; i >= c1; i--) {
+                    ans.push_back(mat[r2][i]);
+                }
+                r2--;
             }
 
-            // We only traverse if it has at least a sningle row
-            if(c1<=c2)
-            {
-            // R2 - R1
-            for(int i=r2;i>=r1;i--)
-            {
-                ans.push_back(matrix[i][c1]);
-            }
-            c1++;
+            // First Column -> r2 -> r1
+            if (c1 <= c2) {
+                for (int i = r2; i >= r1; i--) {
+                    ans.push_back(mat[i][c1]);
+                }
+                c1++;
             }
         }
         return ans;
