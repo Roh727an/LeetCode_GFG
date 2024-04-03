d447dbabffe207c9e55681e64fa92a1c55164f1e
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-03 08:59:52 +0530

    Time: 979 ms (44.10%) | Memory: 11.6 MB (49.58%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-      bool rec(int row,int col,int idx,vector<vector<char>> &board, string word,int n,int m)
-    {
+    bool rec(int row, int col, int idx, vector<vector<char>>& board,
+             string word, int n, int m) {
         // Base Case
-        if(idx==word.length())
+        if (idx == word.length())
             return true;
 
         // Out Of Bound
-        if(row<0 || col <0 || row>=n || col>=m || board[row][col]!=word[idx])
+        if (row < 0 || col < 0 || row >= n || col >= m ||
+            board[row][col] != word[idx])
             return false;
-        bool ans=false;
-            // Change the Caracter to Specail as it is Visited
-            board[row][col]='*';
-            // Check all 4 directions
-            ans=rec(row+1,col,idx+1,board,word,n,m)|| rec(row-1,col,idx+1,board,word,n,m) || rec(row,col+1,idx+1,board,word,n,m) || rec(row,col-1,idx+1,board,word,n,m);
-            // Backtracs
-            board[row][col]=word[idx];
+        bool ans = false;
+        // Change the Caracter to Specail as it is Visited
+        board[row][col] = '*';
+        // Check all 4 directions
+        ans = rec(row + 1, col, idx + 1, board, word, n, m) ||
+              rec(row - 1, col, idx + 1, board, word, n, m) ||
+              rec(row, col + 1, idx + 1, board, word, n, m) ||
+              rec(row, col - 1, idx + 1, board, word, n, m);
+        // Backtracs
+        board[row][col] = word[idx];
         return ans;
     }
     bool exist(vector<vector<char>>& board, string word) {
- 
-       int index=0;
-       int n=board.size();
-       int m=board[0].size();
-    //    Seach each Cell in The Board
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)
-        {
-            if(board[i][j]==word[index])
-            if(rec(i,j,index,board,word,n,m))
-            return true;
+        int index = 0;
+        int n = board.size();
+        int m = board[0].size();
+        //    Seach each Cell in The Board
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == word[index])
+                    if (rec(i, j, index, board, word, n, m))
+                        return true;
+            }
         }
-    }
-    return false;
+        return false;
     }
 };
