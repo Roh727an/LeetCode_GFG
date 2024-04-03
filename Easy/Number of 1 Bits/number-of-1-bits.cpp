dfbec46b303fbd4b38a32f6352cd2b2605bc96fa
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-03 21:24:47 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Number of 1 Bits/number-of-1-bits.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        // Write Your Code here
+        int cnt=0;
+        while(N>1)
+        {
+            cnt+=(N&1);
+            N=N>>1;
+        }
+        if(N==1)
+        cnt++;
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
