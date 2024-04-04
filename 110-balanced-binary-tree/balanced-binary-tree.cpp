a451aa4579f9fe6d24812a0b2261db3aca02a09f
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-04 22:14:18 +0530

    Time: 3 ms (98.10%) | Memory: 21.7 MB (43.50%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -6,37 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    // If any of the Subtree Gives us -1 thats mean that subtree is Not heght
-    // Balanced so we return -1 further without checking anuthing
-    int height(TreeNode* root) {
-        if (root == NULL)
-            return 0;
-        // cout<<root->val<<endl;
-        int left = height(root->left);
-        // Left Subtree is Not Height Balanced
-        if (left == -1)
-            return -1;
+    int rec(TreeNode* root){
+        if(root==NULL)
+        return 0;
 
-        int right = height(root->right);
-        // Right Subtree is Not Height Balanced
-        if (right == -1)
-            return -1;
-        // cout<<"Left "<<left<<" Right "<<right<<endl;
-        // Not Height-Balanced
-        if (abs(left - right) > 1)
-            return -1;
-        // Return height
-        // cout<<"Height is "<<1 + max(left, right)<<endl;
-        // cout<<endl;
-        return 1 + max(left, right);
+        int left=rec(root->left);
+        if(left==-1) // Return Further -1
+        return -1;
+        int right=rec(root->right);
+        if(right==-1) // Return Further -1
+        return -1;
+        
+        if(abs(left-right) > 1)
+        return -1;
+        return 1+max(left,right);
     }
-    bool isBalanced(TreeNode* root) { 
-        return height(root) != -1; 
-        }
+    bool isBalanced(TreeNode* root) {
+        return rec(root)==-1?false:true;
+    }
 };
