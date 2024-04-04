c0b15db90e8012fa2a2a7450b80d58160ec22d10
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-04 22:23:22 +0530

    Time: 3 ms (50.45%) | Memory: 14.6 MB (22.73%) - LeetSync

--- 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
@@ -12,6 +12,22 @@
 class Solution {
 public:
     bool checkTree(TreeNode* root) {
-        return root->val == root->left->val + root->right->val;
+        if(root->left==NULL && root->right==NULL)
+        return true;
+
+        // Calculate Left
+        int left=0;
+        if(root->left)
+        left=root->left->val;
+
+        // Calculate Right
+        int right=0;
+        if(root->right)
+        right=root->right->val;
+
+        if(left+right != root->val)
+        return false;
+
+        return checkTree(root->left) && checkTree(root->right);
     }
 };
