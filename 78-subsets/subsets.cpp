08caf25942ad4b43c18c3048806ed5de741aac8c
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-04 14:43:35 +0530

    Time: 4 ms (40.27%) | Memory: 8.4 MB (54.05%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-void rec(int i,int n,vector<int>& nums,vector<int>temp,vector<vector<int>>&ans)
-{
-    if(i>=n)
-    {
-        ans.push_back(temp);
-        return;
-    }
-    // Pick up
-    temp.push_back(nums[i]);
-    rec(i+1,n,nums,temp,ans);
-    // Backtracks
-    temp.pop_back();
-    // Not Pick
-    rec(i+1,n,nums,temp,ans);
-}
     vector<vector<int>> subsets(vector<int>& nums) {
+        // Bit Manipulation
+        int n=1<<nums.size();
         vector<vector<int>>ans;
-        vector<int>temp;
-        rec(0,nums.size(),nums,temp,ans);
+        for(int i=0;i<n;i++)
+        {
+            vector<int>subset;
+            for(int j=0;j<nums.size();j++)
+            {
+                // Check Set bit ->Pick
+                if(i & (1<<j))
+                subset.push_back(nums[j]);
+            }
+            ans.push_back(subset);
+        }
         return ans;
     }
 };
