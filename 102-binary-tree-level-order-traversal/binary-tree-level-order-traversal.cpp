bc1fbec55c3b847a076f35d7cb02228abdd9c8c9
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-05 11:01:45 +0530

    Time: 7 ms (28.18%) | Memory: 14.9 MB (67.62%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -6,40 +6,31 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        // Create ans
-        vector<vector<int>> ans;
-        if (root == NULL)
-            return ans;
-        // Create a Queue Ds to Store
+        vector<vector<int>>ans;
+        if(root==NULL)
+        return ans;
         queue<TreeNode*>q;
-        // Push the Root Node
         q.push(root);
         while(!q.empty()){
-            // Get the Size / Nodes PResent in the Level
-            int sizeOfLevel=q.size();
-            // Create a Vector to Store the Nodes of A Level
-            vector<int>level;
-            for(int i=0;i<sizeOfLevel;i++)
+            int size=q.size();
+            vector<int>lvl;
+            for(int i=0;i<size;i++) //Level
             {
-                // Get the Front Node from Queue
                 TreeNode* curr=q.front();
                 q.pop();
-                // Push Left & Right Child of the Node
-                if(curr->left) 
+                lvl.push_back(curr->val);
+                if(curr->left)
                 q.push(curr->left);
-                if(curr->right) 
+                if(curr->right)
                 q.push(curr->right);
-                level.push_back(curr->val);
             }
-            // After Completing  A Level make sure to Push the List of That Level into ANS
-            ans.push_back(level);
+            ans.push_back(lvl);
         }
         return ans;
     }
