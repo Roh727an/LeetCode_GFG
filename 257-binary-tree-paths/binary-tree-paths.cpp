6b54559ee23c47d9b7a8b741a98b4bc7b338d6d4
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-05 11:48:28 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (88.10%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -11,27 +11,31 @@
  */
 class Solution {
 public:
-    void rec(TreeNode* root,string ds,vector<string>&ans)
-    {
+    void rec(TreeNode* root,vector<string>&ans,string ds){
+        string d=to_string(root->val);
+        ds+=d;
         if(root->left==NULL && root->right==NULL)
         {
-            ds+=to_string(root->val);
             ans.push_back(ds);
-            return;
+            ds.pop_back();
+            return ;
         }
+        ds+="->";
+        if(root->left) // Move Left if Exist
+        rec(root->left,ans,ds);
+        if(root->right) // Move right if Exist
+        rec(root->right,ans,ds);
 
-        ds+=to_string(root->val);
-        if(root->left)
-        rec(root->left,ds+"->",ans);
-        if(root->right)
-        rec(root->right,ds+"->",ans);
         ds.pop_back();
+        ds.pop_back();
+        ds.pop_back();
+
     }
     vector<string> binaryTreePaths(TreeNode* root) {
-       vector<string>ans;
+      vector<string>ans;  
         if(root==NULL)
         return ans;
-        rec(root,"",ans);
-        return ans;
+      rec(root,ans,"");
+      return ans;
     }
 };
