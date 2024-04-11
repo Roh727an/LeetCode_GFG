175aa4d59c66cb97c47b9dc6eb6284c509c0c564
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-11 15:33:00 +0530

    Time: 7 ms (51.79%) | Memory: 11.9 MB (59.52%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        // Concept of Buckets
-        int single=0,twice=0;
-        for(int i=0;i<nums.size();i++){
-            single=((single^nums[i])&(~twice));
-            twice=((twice^nums[i]) & (~single));
+        int ans=0;
+        // For Bit index
+        for(int b=0;b<32;b++){
+            int countSetBits=0;
+            for(int i=0;i<nums.size();i++)
+            {
+                // Check if i'th bit is set or Not
+                if((nums[i]>>b)&1)
+                countSetBits++;
+            }
+            // If the Set Bit is not Multiple of 3 
+            if(countSetBits%3==1)
+            ans=ans|(1<<b);
         }
-        return single;
+        return ans;
     }
 };
