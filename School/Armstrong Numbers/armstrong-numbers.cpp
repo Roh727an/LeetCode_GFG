7ed4f37f51894f4deeee668f798aca052348fb51
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-16 13:20:28 +0530

    Updated Solution - GfG to GitHub

--- School/Armstrong Numbers/armstrong-numbers.cpp
+++ School/Armstrong Numbers/armstrong-numbers.cpp
@@ -7,27 +7,31 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
+  int digitCount(int n)
+  {
+      int cnt=0;
+      while(n!=0)
+      {
+          cnt++;
+          n=n/10;
+      }
+      return cnt;
+  }
     string armstrongNumber(int n){
         // code here
-        int originalno = n;
-    int count = 0;
-    int temp = n;
-    while (temp != 0)
-    {
-        count++;
-        temp = temp / 10;
-    }
-    int sumofpower = 0;
-    while (n != 0)
-    {
-        int digit = n % 10;
-        sumofpower += pow(digit,count);
-        n /= 10;
-    }
-    if(sumofpower == originalno)
-    return "Yes";
-    else
-    return "No";
+        int totalDig=digitCount(n);
+        int sum=0,temp=n;
+        while(temp!=0){
+            int dig=temp%10;
+            temp=temp/10;
+            sum=sum+ pow(dig,totalDig);
+            if(sum>n)
+                return "No"; 
+        }
+        if(sum<n)
+        return "No"; 
+        else
+        return "Yes"; 
     }
 };
 
