602d002eaef7714840b666d27dd3b2ed89b3bcba
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-19 22:15:54 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (45.49%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-double rec(double x,long long n,double &ans)
-{
-    // Base Case
-    if(n<=0)
-    return ans;
-
-    // n is Odd
-    if(n%2)
-    {
-        ans=ans*x;
-        n=n-1;
-    }
-    else
-    {
-        x=x*x;
-        n=n/2;
-    }
-    return rec(x,n,ans);
-}
-    double myPow(double x, int n) {
-        long long p=abs(n);
-        double ans=1.0;
-        ans=rec(x,p,ans);
-        if(n<0)
-        ans=(double)1.0/ans;
+    double myPow(double x, int N) {
+        double ans=1;
+        int n=abs(N);
+        while(n>0)
+        {
+            if(n%2==1)
+            {
+                ans=ans*x;
+                n=n-1;
+            }
+            else{
+                n=n/2;
+                x=x*x;
+            }
+        }
+        if(N<0)
+        ans=1/ans;
         return ans;
     }
 };
