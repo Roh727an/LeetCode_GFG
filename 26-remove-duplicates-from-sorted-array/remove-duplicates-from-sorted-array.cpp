1ae6a76c179a1bbdb165aa469a522e989d0acbc2
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-22 15:08:33 +0530

    Time: 8 ms (54.85%) | Memory: 21.7 MB (9.23%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        //T.C O(2N) & S.C O(N)
-        /*
-        vector<int>nonDup;
-        nonDup.push_back(nums[0]);
-        // Push all NonDuplicates into nonDup Array
-        for(int i=1;i<nums.size();i++)
+        stack<int>st;
+        st.push(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(!st.empty() && st.top()!=nums[i])
+            st.push(nums[i]);
+        }
+        int size=st.size();
+        for(int i=st.size()-1;i>=0;i--)
         {
-            if(nonDup.back()!=nums[i])
-            nonDup.push_back(nums[i]);
+            nums[i]=st.top();
+            st.pop();
         }
-        // Put back all non duplicates in front of array
-        int k=0;
-        for(k=0;k<nonDup.size();k++){
-            nums[k]=nonDup[k];
-        }
-        return k;
-        */
-        // OPTIMAL SOLUTION -> 2 Pointers approch
-        int nonDup=0;
-        int i=1;
-        for(i=1;i<nums.size();i++)
-        {
-            if(nums[nonDup]!=nums[i])
-            {
-                nums[nonDup+1]=nums[i];
-                nonDup++;
-            }
-        }
-        return nonDup+1;
+        return size;
     }
 };
