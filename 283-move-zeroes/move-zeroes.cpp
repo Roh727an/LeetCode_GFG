a36cf8e328adee238d50c23e1a509300f11fca53
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-22 23:10:37 +0530

    Time: 16 ms (53.07%) | Memory: 21.7 MB (12.11%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& arr) {
-      int zero=1;
-	    int nonZero=0;
-	    while(zero<arr.size() && nonZero<arr.size()){
-	       if(arr[nonZero]==0)
-	        {
-	            if(arr[zero]!=0)
-	            {
-	                swap(arr[zero],arr[nonZero]);
-	                nonZero++;
-	            }
-	            else
-	            zero++;
-	        }
-	        else
-	        {
-	            zero++;
-	            nonZero++;
-	        }
-	    }  
+    void moveZeroes(vector<int>& nums) {
+        int nonZero=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+            swap(nums[i],nums[nonZero]);
+            nonZero++;
+            }
+        }
     }
 };
