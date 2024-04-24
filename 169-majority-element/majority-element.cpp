e1df67cce1348d17816fef8e9862190fa621bb5c
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-04-24 18:13:02 +0530

    Time: 24 ms (16.05%) | Memory: 27.9 MB (12.64%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // Moore's Voting ALgorithm
-        int cnt=0;
-        int ele;
+        unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++)
-        {
-            // Reset
-            if(cnt==0)
-            {
-                cnt=1;
-                ele=nums[i];
-            }
-            else if(nums[i]==ele)
-            cnt++;
-            else
-            cnt--;
+        mp[nums[i]]++;
+
+        for(auto it:mp){
+            if(it.second > nums.size()/2)
+            return it.first;
         }
-        return ele;
+
+        return -1;
     }
 };
