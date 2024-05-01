5872dea86a2ff563ca51d1b874d738e719035ed1
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-05-01 21:33:24 +0530

    Time: 3 ms (41.85%) | Memory: 7.5 MB (59.42%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-        // 1.Create a Stack 
         stack<char>st;
-        
-        for(int i=0;i<s.length();i++)
-        {
-            // IF it is an Opening Bracket
-            // Push the Opening Bracket into The Stack
-            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[' )
             st.push(s[i]);
-
-            // If it is Closing Bracket then check it's Order into the Stack
             else
             {
-                // If there is a Closing Bracket come Before opening one Return false
-            if(st.empty())
-            return false;
-            if((s[i]==')' && st.top()=='(') || (s[i]=='}' && st.top()=='{') || (s[i]==']' && st.top()=='['))
-                st.pop();
-                else
+                if(st.empty())
                 return false;
+                char op=st.top();
+                if((op=='(' && s[i]!=')')||(op=='{' && s[i]!='}') || (op=='[' && s[i]!=']'))
+                return false;
+                else
+                st.pop();    
             }
         }
         return st.empty();
