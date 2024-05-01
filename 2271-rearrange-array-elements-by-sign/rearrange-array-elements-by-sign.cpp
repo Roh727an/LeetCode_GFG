18c35b517f611c4c1d30cb6340bce94b6167addb
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-05-01 17:29:17 +0530

    Time: 154 ms (22.64%) | Memory: 136.9 MB (29.38%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-bool eqalSign(int a,int b){
-    return (a>0 && b>0) || (a<0 && b<0);
-}
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>ans(nums.size(),0);
-        int pos=0,neg=1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>0){
-                ans[pos]=nums[i];
-                pos=pos+2;
-            }
-            else{
-                ans[neg]=nums[i];
-                neg=neg+2;
-            }
+        vector<int>pos,neg;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>=0)
+            pos.push_back(nums[i]);
+            else
+            neg.push_back(nums[i]);
+        }
+        vector<int>ans(nums.size());
+        int p=0,n=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            // Even
+            if(i%2==0)
+            ans[i]=pos[p++];
+            else 
+            ans[i]=neg[n++];
         }
         return ans;
     }
