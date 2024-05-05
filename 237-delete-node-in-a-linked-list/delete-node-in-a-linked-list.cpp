e6fd684184a1bb469d52121ab8dfb406404f8cab
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-05-05 10:54:46 +0530

    Time: 4 ms (80.20%) | Memory: 11.4 MB (43.54%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -9,17 +9,14 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        // Instead of Node Push the Node value to the End & Delete the Last Node
         ListNode* prev=node;
-        ListNode* curr=node;
-        int data=node->val;
-        while(curr->next!=NULL)
-        {
+        ListNode* curr=node->next;
+        while(curr!=NULL){
+            prev->val=curr->val;
+            if(curr->next!=NULL)
             prev=curr;
-            curr->val=curr->next->val;
             curr=curr->next;
         }
-        prev->next=curr->next;
-
+        prev->next=curr;
     }
 };
