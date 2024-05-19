094978bc09e8ca69d6bc21bb2325c7566eec3805
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-05-19 16:48:16 +0530

    Time: 3 ms (62.70%) | Memory: 11.2 MB (76.81%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int el=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[i],nums[el]);
+                el++;
+            }
+        }
+    }
+    vector<int> applyOperations(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+            }
+        }
+        moveZeroes(nums);
+        return nums;
+    }
+};
