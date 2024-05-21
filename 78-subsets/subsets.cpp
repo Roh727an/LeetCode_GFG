f0c4e631e802cdd8c9987b21bc7ddd0a4dc47d43
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-05-21 09:12:18 +0530

    Time: 3 ms (47.78%) | Memory: 15.2 MB (5.39%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
+void rec(int idx,vector<int>nums,vector<int>temp,vector<vector<int>>&ans){
+    if(idx>=nums.size()){
+        ans.push_back(temp);
+        return ;
+    }
+    temp.push_back(nums[idx]);
+    rec(idx+1,nums,temp,ans);
+    temp.pop_back();
+    rec(idx+1,nums,temp,ans);
+}
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n=nums.size();
-        int subsets=(1<<n)-1;
         vector<vector<int>>ans;
-        for(int bitIdx=0;bitIdx<=subsets;bitIdx++)
-        {
-            vector<int>sub;
-            for(int i=0;i<n;i++)
-            {
-                //i'th Bit set so Take the i'th element
-                if((bitIdx>>i)&1)
-                sub.push_back(nums[i]);
-            }
-            ans.push_back(sub);
-        }
+        vector<int>temp;
+        rec(0,nums,temp,ans);
         return ans;
-
     }
 };
