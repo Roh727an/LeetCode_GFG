8cf85510f677f738ac5d8718be99859fb0f0e1ca
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-07-02 18:04:41 +0530

    Time: 5 ms (28.16%) | Memory: 11 MB (71.88%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,69 +1,23 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        /*
-    //    Base Case
-    if (m == 0) {
-        nums1 = nums2;
-        return;
-    }
-    
-    int i = 0;
-    int j = 0;
-    vector < int > ans;
-    // Iterate Till Anyone Goes out of Bound
-    while (i < m && j < n) {
-        if (nums1[i] <= nums2[j]) {
-            ans.push_back(nums1[i]);
-            i++;
+        int i=0,j=0;
+        vector<int>ans;
+        // Compare and Add
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                ans.push_back(nums1[i++]);
+            }
+            else
+            ans.push_back(nums2[j++]);
         }
-        else {
-            ans.push_back(nums2[j]);
-            j++;
+        // Add the Left Ones
+        while(i<m){
+            ans.push_back(nums1[i++]);
         }
-    }
-    // Push Remaining Elements
-    while (i < m) {
-        ans.push_back(nums1[i]);
-        i++;
-    }
-    while (j < n) {
-        ans.push_back(nums2[j]);
-        j++;
-    }
-    nums1 = ans;    
-    */
-    /*
-    // OPTIMISED APPROCH 1
-    // Base Case
-    if (m == 0) {
-        nums1 = nums2;
-        return;
-    }
-    int i=0,j=0;
-
-    while(i<m && j<n)
-    {
-        if(nums1[i] > nums2[j]){
-            swap(nums1[i],nums2[j]);
-            sort(nums2.begin(),nums2.end());
+        while(j<n){
+            ans.push_back(nums2[j++]);
         }
-        i++;
-    }    
-    for(int j=0;j<n;j++)
-    nums1[i+j]=(nums2[j]);
-    */
-    // OPTIMISED APPROCH 2
-    //    Base Case
-    if (m == 0) {
-        nums1 = nums2;
-        return;
-    }
-    // Transfer all Elements into array1
-    for(int i=0;i<n;i++)
-    {
-        nums1[i+m]=nums2[i];
-    }
-    sort(nums1.begin(),nums1.end());
+        nums1=ans;
     }
 };
