4e1532fd4a6a4609dbf4980edf7134caedf8c7ad
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-07-06 21:25:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (32.50%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -17,10 +17,21 @@ public:
 
         return dp[n]=memo(n-1,dp)+memo(n-2,dp);
     }
+    int tabulation(int n){
+        vector<int>dp(n+1,-1);
+        // Base Case
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        dp[i]=dp[i-1]+dp[i-2];
+
+        return dp[n];
+    }
     int climbStairs(int n) {
         // return rec(n);
-        vector<int>dp(n+1,-1);
-        
-        return memo(n,dp);
+        // vector<int>dp(n+1,-1);
+        // return memo(n,dp);
+
+        return tabulation(n);
     }
 };
