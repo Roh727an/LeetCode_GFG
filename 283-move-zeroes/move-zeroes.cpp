d74b1083835d815abc04974915e6d26035757ebf
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-07-08 16:36:48 +0530

    Time: 24 ms (14.05%) | Memory: 21.5 MB (73.83%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,28 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // Base Case
-        if(nums.size()<=1)
-        return ;
-        int i=0,j=0;
-        while(i<nums.size() && j<nums.size()){
-            // Get the first zero 
-            while(i<nums.size() && nums[i]!=0)
-            i++;
-
-            // Fisrt Zero found 
-            j=i+1;
-
-            // Find the first NonZero
-            while(j<nums.size() && nums[j]==0 )
-            j++;
-
-            // First NonZero Found
-            if(i<nums.size() && j<nums.size()){
-            swap(nums[i],nums[j]);
-            i++;
-            j++;
-            }
+        int j=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            swap(nums[i],nums[j++]);
         }
     }
 };
