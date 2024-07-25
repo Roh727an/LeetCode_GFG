f416188ba674795e7e43ef1ba4392da5cd9d60b6
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-07-25 21:20:11 +0530

    Time: 629 ms (20.22%) | Memory: 284.4 MB (19.25%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,63 +1,46 @@
 class Solution {
-public:
-        void merge(vector<int>& arr, int l, int m, int r)
-    {
-        // Your code here
-        //  Create a Temp Array
-        vector<int>temp;
-        // array 1 = arr[l] to arr[m-1]
-        // array2 = arr[m] to arr[r]
-        int i=l;
-        int j=m+1;
-        while(i<=m && j<=r)
-        {
-            if(arr[i]<arr[j])
-            {
-                temp.push_back(arr[i]);
-                i++;
-            }
-            else
-            {
-            temp.push_back(arr[j]);
-            j++;  
-            }
-            
-        }
-        while(i<=m)
-        {
-            temp.push_back(arr[i]);
-            i++;
-        }
-        while(j<=r)
-        {
-            temp.push_back(arr[j]);
-            j++;
-        }
-        for(i=l;i<=r;i++)
-        arr[i]=temp[i-l];
-    }
     public:
-    // Divide the Array into Two Halfs till Array has single element
-    void mergeSort(vector<int>& arr, int l, int r)
+    void merge(vector<int>&arr, int l, int m, int r)
+    {
+         // Your code here
+         int i=l,j=m+1;
+         vector<int>temp;
+         while(i<=m && j<=r)
+         {
+             if(arr[i]<arr[j])
+                 temp.push_back(arr[i++]);
+             else
+              temp.push_back(arr[j++]);
+         }
+         while(i<=m)
+         {
+            temp.push_back(arr[i++]);
+         }
+         while(j<=r)
+         {
+            temp.push_back(arr[j++]);
+         }
+         
+         for(int i=l;i<=r;i++)
+         arr[i]=temp[i-l];
+         
+    }
+    void mergeSort(vector<int>&arr, int l, int r)
     {
         //code here
-        // Base Case-> Single element
         if(l>=r)
-        return ;
-        // Calculate middle of Array
-        int mid=(l+r)/2;
-        // Recursion Call One - > Array 1 
+        return;
+        // Calculate mid
+        int mid=(l+r)>>1;
+        // Left Recursive Call
         mergeSort(arr,l,mid);
-        // Recursion Call Two - > Array 2 
+        // Right Recursive Call
         mergeSort(arr,mid+1,r);
         
-        // After Dividing ,it's Time for Merging
         merge(arr,l,mid,r);
     }
     vector<int> sortArray(vector<int>& nums) {
-        vector<int>ans=nums;
-        int n=nums.size();
-        mergeSort(ans,0,n-1);
-        return ans;
+        mergeSort(nums,0,nums.size()-1);
+        return nums;
     }
 };
