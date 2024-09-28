bab9d316849bf77e3cd460b4345db7872797b98c
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-09-28 15:43:40 +0530

    Time: 570 ms (26.87%) | Memory: 284.3 MB (19.37%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,43 +1,34 @@
 class Solution {
-    public:
-    void merge(vector<int>&arr, int l, int m, int r)
-    {
-         // Your code here
-         int i=l,j=m+1;
-         vector<int>temp;
-         while(i<=m && j<=r)
-         {
-             if(arr[i]<arr[j])
-                 temp.push_back(arr[i++]);
+public:
+void merge(vector<int>& arr, int l, int m,int r) {
+        vector<int>a;
+         int i=l;
+         int j=m+1;
+         while(i<=m && j<=r){
+             if(arr[i]<arr[j]){
+                 a.push_back(arr[i++]);
+             }
              else
-              temp.push_back(arr[j++]);
+             {
+                 a.push_back(arr[j++]);
+             }
          }
          while(i<=m)
-         {
-            temp.push_back(arr[i++]);
-         }
+         a.push_back(arr[i++]);
          while(j<=r)
-         {
-            temp.push_back(arr[j++]);
-         }
+         a.push_back(arr[j++]);
          
-         for(int i=l;i<=r;i++)
-         arr[i]=temp[i-l];
-         
+         for(i=l;i<=r;i++)
+         arr[i]=a[i-l];
     }
-    void mergeSort(vector<int>&arr, int l, int r)
-    {
-        //code here
+    void mergeSort(vector<int>& nums,int l,int r){
         if(l>=r)
-        return;
-        // Calculate mid
-        int mid=(l+r)>>1;
-        // Left Recursive Call
-        mergeSort(arr,l,mid);
-        // Right Recursive Call
-        mergeSort(arr,mid+1,r);
-        
-        merge(arr,l,mid,r);
+        return ;
+
+        int mid=(l+r)/2;
+        mergeSort(nums,l,mid);
+        mergeSort(nums,mid+1,r);
+        merge(nums,l,mid,r);
     }
     vector<int> sortArray(vector<int>& nums) {
         mergeSort(nums,0,nums.size()-1);
