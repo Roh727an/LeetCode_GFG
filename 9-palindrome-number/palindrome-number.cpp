ac20fd35f522a7b5d21e14381351a937621290dc
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-10-02 10:04:55 +0530

    Time: 4 ms (85.06%) | Memory: 8.5 MB (76.80%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-long long reverseNumber(int n){
-    long long rev=0;
-    while(n>0 ){
-        int rem=n%10;
-        rev=rev*10 + rem;
-        n=n/10;
+    int revNumber(int n){
+        long long rev=0;
+        while(n>0)
+        {
+            rev=rev*10 + (n%10);
+            n=n/10;
+        }
+        return rev;
     }
-    return rev;
-}
-    bool isPalindrome(int n) {
-    if(n<0 || n>2147483647)
-    return false;
-    if(n<=9)
-    return true;
-    // Reverse the Number
-    long long revNum=reverseNumber(n);
-    return n==revNum;
+    bool isPalindrome(int x) {
+        if(x<0)
+        return false;
+
+        long long rev=revNumber(x);
+
+        return x==rev;
     }
 };
