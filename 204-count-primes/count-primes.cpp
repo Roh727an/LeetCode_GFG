ab8f30563181c7b20106ce9605b1072f7b00ea40
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-10-29 18:48:36 +0530

    Time: 527 ms (18.69%) | Memory: 11.6 MB (89.33%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        // Sieve Of Eratosthenes Algorithm
-        int cnt=0;
-        long prime[n+1];
-        // Mark all As Prime
-        for(int i=2;i<n;i++)
-        prime[i]=1;
+        // Base Case
+        if (n == 0)
+            return 0;
 
-        for(long i=2;i*i<n;i++)
-        {
-            if(prime[i]==1)
-            {
-                // Mark all Multiples as Not Prime
-                for(long long j=i*i;j<=n;j+=i)
-                prime[j]=0;
+        vector<bool> primes(n + 1, true);
+        primes[0] = primes[1] = false;
+        int cnt = 0;
+
+        for (int i = 2; i < n; i++) {
+
+            // If Prime -> Mark Multiples as Non Prime
+            if (primes[i]) {
+                cnt++;
+
+                for (int j = 2 * i; j < n; j += i)
+                    primes[j] = 0;
             }
-        }
-        for(int i=2;i<n;i++)
-        {
-            cnt+=prime[i];
         }
         return cnt;
     }
