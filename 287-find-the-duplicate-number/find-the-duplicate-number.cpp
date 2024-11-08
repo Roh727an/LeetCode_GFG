2fe7ddd35556c6c064288f025259e65d91335585
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-11-08 16:20:56 +0530

    Time: 4 ms (63.88%) | Memory: 66.4 MB (22.72%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int dup = 0;
-        for (int bit = 0; bit < 32; bit++) {
-            // Check if I'th Bit is Set or Not & Count
-            int orgSet = 0, arrSet = 0;
-            for (int i = 0; i < nums.size(); i++) {
-                if ((1 << bit) & i)
-                    orgSet++;
-                if ((1 << bit) & nums[i])
-                    arrSet++;
-            }
-            // Unset the i'th Bit if the Number of 'th Bit is Greater than Org
-            if (arrSet > orgSet)
-                dup |=(1 << bit);
+        vector<int>hash(nums.size()+1,0);
+        for(int i=0;i<nums.size();i++){
+            hash[nums[i]]++;
+            if(hash[nums[i]]>1)
+            return nums[i];
         }
-        return dup;
+        return -1;
     }
 };
