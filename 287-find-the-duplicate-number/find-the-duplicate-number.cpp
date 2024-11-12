c47ee9a3d0272d89b8ef46706681024e4bd5e0f4
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-11-12 16:55:02 +0530

    Time: 12 ms (47.81%) | Memory: 63.9 MB (53.06%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int ans=-1;
-        for(int i=0;i<nums.size();i++){
-            int idx=abs(nums[i]);
-            // If Visited
-            if(nums[idx]<0)
-            {
-                ans=idx;
-                break;
-            }
-            // Mark as Visited
-            nums[idx]*=(-1);
+        int slow=nums[0],fast=nums[0];
+        // Detect Cycle
+        do{
+            slow=nums[slow]; //+1
+            fast=nums[nums[fast]]; //+2
+        }while(slow!=fast);
+
+        // Detect Staring Point(Duplicate)
+        slow=nums[0];
+        while(slow!=fast){
+            slow=nums[slow]; //+1
+            fast=nums[fast]; //+1
         }
-        return ans;
+        return slow;
     }
 };
