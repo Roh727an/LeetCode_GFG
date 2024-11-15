30a78824f7f91c8eed6b021f73801c7e04a63d48
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-11-15 16:28:54 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (92.56%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& mat, int X) {
-        int n=mat.size();
-        int m=mat[0].size();
-        int s=0;
-	    int e=n*m-1;
-	    while(s<=e){
-	        int mid=(s+e)/2;
-	       // Get the row & col of Mid from 2d Matrix
-	        int mid_row=mid/m;
-	        int mid_col=mid%m;
-	        
-	        if(mat[mid_row][mid_col]==X)
-	        return 1;
-	        
-	        else if(mat[mid_row][mid_col]>X)
-	        e=mid-1;
-	        else
-	        s=mid+1;
-	    }
-	    return 0;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size(),m=matrix[0].size();
+        int s=0,e=m*n-1,mid,r,c,element;
+        while(s<=e){
+            mid=s+(e-s)/2;
+            r=mid/m;
+            c=mid%m;
+            element=matrix[r][c];
+            // Binary Search Conditions
+            if(element==target)
+            return true;
+            else if(element>target)
+            e=mid-1;
+            else
+            s=mid+1;
+        }
+        return false;
     }
 };
