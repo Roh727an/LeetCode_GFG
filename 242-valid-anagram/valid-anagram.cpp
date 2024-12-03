09e93d787178135dbea2f5517797b3ff6a1abf54
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2024-12-03 17:50:14 +0530

    Time: 8 ms (16.11%) | Memory: 9.6 MB (44.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,17 +3,11 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length())
         return false;
-        int freq[26]={0};
-        // Hash the first String into Freq Array
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
         for(int i=0;i<s.length();i++)
         {
-            freq[s[i]-'a']++;
-            freq[t[i]-'a']--;
-        }
-        // Check the Frquenceis
-        for(int i=0;i<26;i++)
-        {
-            if(freq[i]!=0)
+            if(s[i]!=t[i])
             return false;
         }
         return true;
